9558ab771a5679a2e0efd9e00cf2778f757f4be9
Niraj Tiwari <tniraj7@gmail.com>
2020-11-01 20:59:41 +0900

    display list of videos when tab bar watch menu clicked

--- AfricaSafari/AfricaSafari/App/VideoListView.swift
+++ AfricaSafari/AfricaSafari/App/VideoListView.swift
@@ -1,11 +1,37 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK:- Properties
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
+    // MARK:- Body
     var body: some View {
-        Text("Video")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        videos.shuffle()
+                        hapticImpact.impactOccurred()
+                    }, label: {
+                        Image(systemName: "arrow.2.squarepath")
+                    })
+                }
+            }
+        }
     }
 }
 
+
+// MARK:- Preview
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
