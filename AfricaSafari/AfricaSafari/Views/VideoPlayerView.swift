71eedd7ea44fc7b798fad85d3c19be5df0c0ae71
Niraj Tiwari <tniraj7@gmail.com>
2020-11-02 18:55:04 +0900

    make VidePlayerView

---
+++ AfricaSafari/AfricaSafari/Views/VideoPlayerView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    // MARK:- Properties
+    var videoSelected: String
+    var videoTitle: String
+    
+    // MARK:- Body
+    var body: some View {
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4"))
+            .overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top, 6)
+                    .padding(.horizontal, 8)
+                , alignment: .topLeading
+            )
+        }
+        .accentColor(.accentColor)
+        .navigationBarTitle(videoTitle, displayMode: .inline)
+    }
+}
+
+
+// MARK:- Preview
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+        }
+    }
+}
