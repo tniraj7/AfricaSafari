b3f2d02bf1dff7abfd8fcaa570b7e3e8026fad00
Niraj Tiwari <tniraj7@gmail.com>
2020-11-04 01:10:33 +0900

    make grid of animals using LazyVGrid and display selected animal using state property wrapper

--- AfricaSafari/AfricaSafari/App/GalleryView.swift
+++ AfricaSafari/AfricaSafari/App/GalleryView.swift
@@ -1,11 +1,37 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK:- Properties
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let gridlLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    @State private var selectedAnimal: String = "lion"
     
     // MARK:- Body
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Placeholder")
+            VStack(alignment: .center, spacing: 30.0) {
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                LazyVGrid(columns: gridlLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    }
+                }
+            }
+            .padding(.horizontal, 10.0)
+            .padding(.vertical, 50.0)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
