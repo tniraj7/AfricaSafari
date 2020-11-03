91e1c23bd9e42410a29f03c14252257bff249012
Niraj Tiwari <tniraj7@gmail.com>
2020-11-04 00:52:07 +0900

    Use MotionAnimationView in Gallery

--- AfricaSafari/AfricaSafari/App/GalleryView.swift
+++ AfricaSafari/AfricaSafari/App/GalleryView.swift
@@ -1,13 +1,21 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    // MARK:- Body
     var body: some View {
-        Text("GalleryView")
+        ScrollView(.vertical, showsIndicators: false) {
+            Text("Placeholder")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
 
+// MARK:- Preview
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
         GalleryView()
+            .previewDevice("iPhone 11")
     }
 }
