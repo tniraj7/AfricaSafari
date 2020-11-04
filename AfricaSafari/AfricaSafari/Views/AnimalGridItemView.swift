0f5dffb027a5010c0960f284741b7de991f7557f
Niraj Tiwari <tniraj7@gmail.com>
2020-11-05 00:11:05 +0900

    add AnimalGridItemView

---
+++ AfricaSafari/AfricaSafari/Views/AnimalGridItemView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct AnimalGridItemView: View {
+    // MARK:- Properties
+    var animal: Animal
+    
+    // MARK:- Body
+    
+    var body: some View {
+        Image(animal.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+// MARK:- Preview
+struct AnimalGridItemView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        AnimalGridItemView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
