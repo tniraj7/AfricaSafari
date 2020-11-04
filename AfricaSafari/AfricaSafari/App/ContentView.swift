156dc97e8a0c9f3a29b031aea3426dfaf29e97bc
Niraj Tiwari <tniraj7@gmail.com>
2020-11-05 00:11:49 +0900

    display GridView when grid button is pressed, navigation to AnimalDetailView when grid item is pressed

--- AfricaSafari/AfricaSafari/App/ContentView.swift
+++ AfricaSafari/AfricaSafari/App/ContentView.swift
@@ -5,18 +5,36 @@ struct ContentView: View {
     let animals: [Animal] = Bundle.main.decode("animals.json")
     let haptics = UIImpactFeedbackGenerator(style: .medium)
     @State private var isGridViewActive: Bool = false
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
     
     // MARK:- Body
     var body: some View {
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            }
+                        }
                     }
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                }
+                            }
+                        }
+                    }
+                    .padding(10)
+                    .animation(.easeIn)
                 }
             }
             .navigationBarTitle("Africa", displayMode: .large)
