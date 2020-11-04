64b413961de9bbf5b4a535f9c2fb75e86ba2c2be
Niraj Tiwari <tniraj7@gmail.com>
2020-11-04 23:53:11 +0900

    add Toolbar buttons to ContentView for switching between list and gridview

--- AfricaSafari/AfricaSafari/App/ContentView.swift
+++ AfricaSafari/AfricaSafari/App/ContentView.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct ContentView: View {
     // MARK:- Properties
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    @State private var isGridViewActive: Bool = false
     
     // MARK:- Body
     var body: some View {
@@ -18,6 +20,31 @@ struct ContentView: View {
                 }
             }
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        Button(action: {
+                            print("List view activated")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        
+                        Button(action: {
+                            print("Grid view activated")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                    }
+                }
+            }
         }
     }
 }
