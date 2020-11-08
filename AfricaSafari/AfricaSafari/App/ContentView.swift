67f6b0e6598937f15dac1fae5082494f4720a36d
Niraj Tiwari <tniraj7@gmail.com>
2020-11-08 17:00:00 +0600

    change no. of grid coulmns upon grid button click

--- AfricaSafari/AfricaSafari/App/ContentView.swift
+++ AfricaSafari/AfricaSafari/App/ContentView.swift
@@ -5,7 +5,26 @@ struct ContentView: View {
     let animals: [Animal] = Bundle.main.decode("animals.json")
     let haptics = UIImpactFeedbackGenerator(style: .medium)
     @State private var isGridViewActive: Bool = false
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Int = 1
+    @State var toolbarIcon: String = "square.grid.2x2"
+    
+    // MARK:- Functions
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
     
     // MARK:- Body
     var body: some View {
@@ -53,8 +72,9 @@ struct ContentView: View {
                         Button(action: {
                             isGridViewActive = true
                             haptics.impactOccurred()
+                            gridSwitch()
                         }) {
-                            Image(systemName: "square.grid.2x2")
+                            Image(systemName: toolbarIcon)
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .accentColor : .primary)
                         }
