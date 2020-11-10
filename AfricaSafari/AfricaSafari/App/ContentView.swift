e0f3efafa52f0c531bede3cd200d7ef72c01e25d
Niraj Tiwari <tniraj7@gmail.com>
2020-11-10 18:30:45 +0900

    apply CenterModifier to CreditsView()

--- AfricaSafari/AfricaSafari/App/ContentView.swift
+++ AfricaSafari/AfricaSafari/App/ContentView.swift
@@ -43,6 +43,7 @@ struct ContentView: View {
                         }
                         
                         CreditsView()
+                            .modifier(CenterModifier())
                     }
                 } else {
                     ScrollView(.vertical, showsIndicators: false) {
